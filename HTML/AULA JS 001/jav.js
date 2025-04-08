036f199d73d7803d4c675e68627e1bc82837b31b
Akiranim <joaolucassoaresn@gmail.com>
2025-04-08 11:14:51 -0300

    Sistema

---
+++ HTML/AULA JS 001/jav.js
@@ -0,0 +1,27 @@
+document.getElementById("loginForm").addEventListener("submit", function(event){
+    event.preventDefault(); 
+
+    const username = document.getElementById("username").value;
+const password = document.getElementById("password").value;
+
+// Validação de campos
+
+if(!username || !password){
+    alert("Por favor, preencha todos os campos.");
+    return;
+}
+
+if(password.length < 8){
+    alert("A senha deve ter pelo menos 8 caracteres.");
+    return;
+}
+
+// Salvar nome do usuário no localstorage
+localStorage.setItem("username",username);
+
+window.location.href = "painel.html";
+alert("Login bem sucedido!");
+
+});
+
+
